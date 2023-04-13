657532cf4ca8ccb5d7898e8bb17cdee316293838
Kiprono Festus Koech <39343170+FestusKoech@users.noreply.github.com>
2023-04-13 09:11:40 +0300

    Add files via upload
    Updated the database

--- logout.php
+++ logout.php
@@ -1,13 +1,29 @@
-<?php
-session_start();
-include("includes/config.php");
-$_SESSION['login']=="";
-date_default_timezone_set('Asia/Kolkata');
-$ldate=date( 'd-m-Y h:i:s A', time () );
-mysqli_query($con,"UPDATE userlog  SET logout = '$ldate' WHERE userEmail = '".$_SESSION['login']."' ORDER BY id DESC LIMIT 1");
-session_unset();
-$_SESSION['errmsg']="You have successfully logout";
-?>
-<script language="javascript">
-document.location="index.php";
-</script>
+<?php 
+require_once 'include/initialize.php';
+// Four steps to closing a session
+// (i.e. logging out)
+
+// 1. Find the session
+@session_start();
+
+// 2. Unset all the session variables
+unset( $_SESSION['CUSID']);
+// unset( $_SESSION['USERID'] );
+unset( $_SESSION['CUSNAME'] );
+unset( $_SESSION['CUSUNAME'] );
+unset( $_SESSION['CUSUPASS'] ); 
+unset($_SESSION['gcCart']);
+unset($_SESSION['gcCart']);
+unset( $_SESSION['fixnmixConfiremd']);
+unset($_SESSION['gcNotify']);
+unset($_SESSION['orderdetails']);
+
+// unset($_SESSION['FIRSTNAME']);
+// unset($_SESSION['LASTNAME']);
+// unset($_SESSION['ADDRESS']);
+// unset($_SESSION['CONTACTNUMBER']);
+ 	
+// 4. Destroy the session
+// session_destroy();
+redirect("index.php?logout=1");
+?> 	 
